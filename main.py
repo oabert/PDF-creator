e5034c6319c2008a9654bbc4b1d2c79464f214ae
Olena Abert <oabert@gmail.com>
2023-11-18 11:13:06 -0500

    Add multiple lines

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=24, txt=row['Topic'], align='L', ln=1)
     pdf.line(10, 27, 200, 27)
+    # Multiple lines
+    for line in range(34, 270, 7):
+        pdf.set_draw_color(100, 100, 100)
+        pdf.line(10, line, 200, line)
 
     # Footer
     pdf.ln(250)
@@ -30,5 +34,10 @@ for index, row in df.iterrows():
         pdf.set_font(family='Times', style='I', size=10)
         pdf.set_text_color(100, 100, 100)
         pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
+        # Multiple lines
+        for line in range(27, 270, 7):
+            pdf.set_draw_color(100, 100, 100)
+            pdf.line(10, line, 200, line)
+
 
 pdf.output('output.pdf')
